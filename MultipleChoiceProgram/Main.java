53c9f47394bd00f21be8e66dffc9c9de38626bf4
Henry Chung <h23chung@uwaterloo.ca>
2014-07-30 15:15:47 -0400

    Cleaned up Main a little bit. Now it's easy to change the list of test files to load. Future commit will make list of test files separate from code to prevent recompiling.

--- MultipleChoiceProgram/Main.java
+++ MultipleChoiceProgram/Main.java
@@ -10,41 +10,34 @@ public class Main {
 	
 	public static void main(String[] args) {
 		ArrayList<MultipleChoiceQuestion> questions = new ArrayList<MultipleChoiceQuestion>();
-		if(args.length > 0) {
-			try {
-				for(int i = 0; i < args.length; i++) {
-					int chapter = Integer.parseInt(args[i]);
-					questions.addAll(QuestionParser.parseFile(
-							new File("Ch" + chapter + ".dat"))
-						);
-				}
-			} catch(Exception e) {
-				System.out.println(
-						"Invalid arguments: Arguments should be a list of chapters. " +
-						"Right now, only chapters 24, 25, and 26 are supported."
-					);
-				System.exit(0);
-			}
-		} else {
-			try {
-				Scanner scanner = new Scanner(new File("resources/testcases.json"));
+		String[] questionFilenames = Main.getQuestionFilenames();
+		
+		try {
+			Scanner scanner;
+			Type listType = new TypeToken<ArrayList<MultipleChoiceQuestion>>() {
+	              	      }.getType();
+			for(int i = 0; i < questionFilenames.length; i++) {
+				scanner = new Scanner(new File(questionFilenames[i]));
 				String json = scanner.useDelimiter("\\Z").next();
 				scanner.close();
+
 				Gson gson = new Gson();
-				Type listType = new TypeToken<ArrayList<MultipleChoiceQuestion>>() {
-		              	      }.getType();
-				ArrayList<MultipleChoiceQuestion> selectedQuestions = gson.fromJson(json, listType);
-				questions.addAll(selectedQuestions);
-			} catch (Exception e) {
-				//TODO: Handle file not found
-				System.out.println(e);
-				return;
+				ArrayList<MultipleChoiceQuestion> questionsFromFile = gson.fromJson(json, listType);
+				questions.addAll(questionsFromFile);
 			}
+		} catch (Exception e) {
+			//TODO: Handle file not found
+			System.out.println(e);
+			return;
 		}
 
 		System.out.printf("%d Questions Loaded!\n", questions.size());
-
 		MultipleChoiceGUI prog = new MultipleChoiceGUI(questions);
 	}
 
+	public static String[] getQuestionFilenames() {
+		return new String[] {
+			"resources/testcases.json"
+		};
+	}
 }
